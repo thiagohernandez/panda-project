3dfa6e2b9ddd085a320a55ebf8ee90c3be335078
Thiago Hernandez <thiago.hernandez@gmail.com>
2018-11-05 21:48:33 +0100

    Update dashboard

--- js/frontend.js
+++ js/frontend.js
@@ -191,6 +191,23 @@ jQuery(document).ready(function() {
 	if (pathname == "/"){
 		$("#home_header").addClass("active");
 	}
+
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd = '0'+dd
+    } 
+
+    if(mm<10) {
+        mm = '0'+mm
+    } 
+
+    today = mm + '/' + dd + '/' + yyyy;
+    $('.printDateJS').text(today);
 					
 });	//ready
 
