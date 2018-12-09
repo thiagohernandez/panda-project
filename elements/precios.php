ecee8815eb0d8c586a1dd7d57eee56cd276be30b
Thiago Hernandez <thiago.hernandez@gmail.com>
2018-12-09 21:45:56 +0100

    Update pricing tables

--- elements/precios.php
+++ elements/precios.php
@@ -10,8 +10,8 @@
 					<h5 class="light no-pm"><?= txt('particular') ?></h5>
 					<!-- Price -->
 					<div class="pricing-tables__price">
-						<p class="tachado">29,90</p>
-						<h3><span class="text-lg2 extrabold">9,90</span>€</h3>
+						<p class="tachado">29<small>,90€</small></p>
+						<h3><span class="text-lg2 extrabold">9</span>,90€</h3>
 					</div>
 				</div>
 				<!-- List -->
@@ -24,15 +24,76 @@
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('estadisticasdesde') ?></li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('emaildiario') ?> </li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('pushdesenales') ?>  </li>
-					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('posibilidaddeelegir') ?> </li>
+					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('posibilidaddeelegir') ?> <?= txt('conlaposibilidad') ?></li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('rentabilidadestimada') ?> </li>
 				</ul>
 				<!-- Button -->
 				<div class="xs-my xs-pl xs-pr">
-					<!-- Button -->
-					<a href="https://www.paypal.com/webapps/shoppingcart?flowlogging_id=9fc81960d2502&mfid=1534247674514_9fc81960d2502#/checkout/openButton" class="btn btn-block btn-primary slow"><?= txt('comprar') ?></a>
+					<!-- Button trigger modal -->
+					<a href="#pricingTables01" class="btn btn-block btn-primary slow" data-toggle="modal" data-target="#pricingTables01">
+						<?= txt('comprar') ?>
+					</a>
 				</div>
 			</div>
+		</div>
+
+		<div class="modal" tabindex="-1" role="dialog" id="pricingTables01">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title"><?= txt('pro') ?> <small><?= txt('particular') ?></small></h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		      		<div class="row">
+		      			<div class="col-md-4">
+							<div class="jumbotron bg-gray1 bs-sm">
+		                        <div>
+		                            <h2 class="bold-title text-center">TOP 10</h2>
+		                            <div class="pricing-tables__price">
+										<p class="tachado">29<small>,90€</small></p>
+										<h3><span class="text-lg2 extrabold">9</span>,90€</h3>
+									</div>
+									<p class="text-center">
+										<a class="btn btn-block btn-primary slow" href="#" role="button"><?= txt('comprar') ?></a>
+									</p>
+		                        </div>
+		                    </div>
+	                    </div>
+	                    <div class="col-md-4">
+							<div class="jumbotron bg-gray1 bs-sm">
+		                        <div>
+		                            <h2 class="bold-title text-center">TOP 10 + 5 compañías</h2>
+		                            <div class="pricing-tables__price">
+										<p class="tachado">19<small>,90€</small></p>
+										<h3><span class="text-lg2 extrabold">39</span>,90€</h3>
+									</div>
+									<p class="text-center">
+										<a class="btn btn-block btn-primary slow" href="#" role="button"><?= txt('comprar') ?></a>
+									</p>
+		                        </div>
+		                    </div>
+	                    </div>
+	                    <div class="col-md-4">
+							<div class="jumbotron bg-gray1 bs-sm">
+		                        <div>
+		                            <h2 class="bold-title text-center">TOP 10 + todo el mercado</h2>
+		                            <div class="pricing-tables__price">
+										<p class="tachado">29<small>,90€</small></p>
+										<h3><span class="text-lg2 extrabold">64</span>,80€</h3>
+									</div>
+									<p class="text-center">
+										<a class="btn btn-block btn-primary slow" href="#" role="button"><?= txt('comprar') ?></a>
+									</p>
+		                        </div>
+		                    </div>
+	                    </div>
+                    </div>
+		      </div>
+		    </div>
+		  </div>
 		</div>
 
 									  
@@ -43,19 +104,19 @@
 			<div class="border-1 border-gray bs-light-hover slow">
 				<!-- Box Top -->
 				<div class="bg-gray1 xs-py">
-					<h4 class="extrabold no-pm white"><?= txt('goldcontinent') ?></h4>
-					<h5 class="light no-pm white"><?= txt('brokersfamily') ?></h5>
+					<h4 class="extrabold no-pm"><?= txt('goldcontinent') ?></h4>
+					<h5 class="light no-pm"><?= txt('brokersfamily') ?></h5>
 					<!-- Price -->
 					<div class="pricing-tables__price">
-						<p class="tachado">289,00</p>
-						<h3><span class="text-lg2 extrabold">99,90</span>€</h3>
+						<p class="tachado">289<small>,00€</small></p>
+						<h3><span class="text-lg2 extrabold">99</span>,90€</h3>
 					</div>
 				</div>
 				<!-- List -->
 				<ul class="basic-list list-lg xs-my">
 
 				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <strong><?= txt('1continente') ?></strong> <?= txt('atueleccion') ?></li>
-				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('top10') ?> <?= txt('decadamercado') ?></li>
+				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('top20') ?> <?= txt('decadamercado') ?> <?= txt('plus30') ?></li>
 				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('garantiadedevolucion') ?> </li>
 				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('15minantes') ?></li>
 				  <li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('stopsautomaticos') ?></li>
@@ -92,15 +153,15 @@
 					<h5 class="light no-pm"><?= txt('grandesfamily') ?></h5>
 					<!-- Price -->
 					<div class="pricing-tables__price">
-						<p class="tachado">289,00</p>
-						<h3><span class="text-lg2 extrabold">99,90</span>€</h3>
+						<p class="tachado">1.489<small>,00€</small></p>
+						<h3><span class="text-lg2 extrabold">789</span>,00€</h3>
 					</div>
 				</div>
 				<!-- List -->
 				<ul class="basic-list list-lg xs-my">
 
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <strong> <?= txt('todoslosmercados') ?></strong> </li>
-					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('top10') ?> <?= txt('decadamercado') ?> <?= txt('delmundo') ?></li>
+					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('top30') ?> <?= txt('decadamercado') ?> <?= txt('delmundo') ?> <?= txt('plus30') ?></li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('garantiadedevolucion') ?> </li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('30minantes') ?></li>
 					<li class="px-26"><small class="text-green"><i class="fas fa-check"></i></small> <?= txt('stopsautomaticos') ?></li>
@@ -113,17 +174,8 @@
 				</ul>
 				<!-- Button -->
 				<div class="xs-my xs-pl xs-pr">
-					<div class="col text-left">
-						<div class="classic_checkbox radio radioClubInverters">
-							<input type="radio" name="radioclub" id="radioClubOne" value="6 <?= txt('meses') ?>" checked=""> <label for="radioClubOne"><small>6 <?= txt('meses') ?>/182€</small></label>
-						</div>
-						<div class="classic_checkbox radio radioClubInverters">
-							<input type="radio" name="radioclub" id="radioClubTwo" value="1 <?= txt('ano') ?>"> <label for="radioClubTwo"><small>1 <?= txt('ano') ?>/364€</small></label>
-						</div>
-					</div>
 					<!-- Button -->
 					<a href="#club6meses" class="btn btn-block btn-primary club6months"><?= txt('comprar') ?></a>
-					<a href="#club1ano" class="btn btn-block btn-primary club1year" style="display:none;"><?= txt('comprar') ?></a>
 				</div>
 			</div>
 		</div>
